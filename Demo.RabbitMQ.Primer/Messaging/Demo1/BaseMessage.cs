63cd71dbaf789be5c085080e39fd7258d2110ac4
Nikolay Bulba <nbulba@gmail.com>
2017-12-09 16:09:46 +1100

    initial commit

---
+++ Demo.RabbitMQ.Primer/Messaging/Demo1/BaseMessage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.RabbitMQ.Primer.Messaging.Demo1
+{
+    public enum MessageType
+    {
+        Unknown = 0,
+        Morning,
+        Afternoon,
+        Evening,
+        Night,
+    }
+
+    public class QueueMessageContainer<T>
+    {
+        public MessageType Type { get; set; }
+
+        public T Message { get; set; }
+    }
+
+    public class MorningMessage
+    {
+        public string Text { get; set; } = "Morning";
+    }
+
+    public class EveningMessage
+    {
+        public string Text { get; set; } = "Evening";
+    }
+}
---
+++ Demo.RabbitMQ.Primer/Program.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace Demo.RabbitMQ.Primer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory =
+                new ConnectionFactory
+                {
+                    HostName = "sidewinder.rmq.cloudamqp.com",
+                    UserName = "aezrrkyw",
+                    Password = "***",
+                    VirtualHost = "aezrrkyw",
+                };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare("exch.demo1.incoming", "direct");
+
+                    channel.QueueDeclare("q.demo1.bob", false, false, false, null);
+                    channel.QueueBind("q.demo1.bob", "exch.demo1.incoming", "demo1.bob");
+
+                    channel.QueueDeclare("q.demo1.ana", false, false, false, null);
+                    channel.QueueBind("q.demo1.ana", "exch.demo1.incoming", "demo1.ana");
+
+                    channel.QueueDeclare("q.demo1.tom", false, false, false, null);
+                    channel.QueueBind("q.demo1.tom", "exch.demo1.incoming", "demo1.tom");
+
+                    channel.QueueDeclare("q.demo1.flo", false, false, false, null);
+                    channel.QueueBind("q.demo1.flo", "exch.demo1.incoming", "demo1.flo");
+
+                    var message = JsonConvert.SerializeObject(new Messaging.Demo1.EveningMessage()); ;
+                    var payload = Encoding.UTF8.GetBytes(message);
+
+                    //foreach (var receiver in new[] { "xxx" })
+                    foreach (var receiver in new[] { "bob", "ana", "tom", "flo" })
+                    {
+                        channel.BasicPublish("exch.demo1.incoming", $"demo1.{receiver}", null, payload);
+                    }
+                }
+            }
+        }
+    }
+}
